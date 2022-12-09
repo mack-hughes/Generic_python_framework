420f58ed1e5406349c9969f0298ced25be39d2b1
mack hughes <mackhughes251@gmail.com>
2022-12-09 13:55:18 -0300

    config: Setting project configs

---
+++ src/start.py
@@ -0,0 +1,17 @@
+"""
+    The main file
+"""
+
+
+def sum_function(number_1: float, number_2: float) -> float:
+    """summing two numbers
+
+    Args:
+        number_1 (float): any number in the reals
+        number_2 (float): any number in the reals
+
+    Returns:
+        float: a number that belongs to the real
+    """
+    sum_two_numbers = number_1 + number_2
+    return sum_two_numbers
---
+++ src/start_test.py
@@ -0,0 +1,8 @@
+from .start import sum_function
+
+def test_sum_function():
+    """
+        Testing the sum function
+    """
+    result = sum_function(1, 1)
+    assert result == 2
